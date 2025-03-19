6724f4a9018bed16dadd59d7d848fbe07c4ed3bf
Jeff <duck1314521@gmail.com>
2025-03-19 13:13:15 +0800

    🐾 喵醬升級！加入台語排行榜 & 音樂播報撒嬌語錄 🎶😻

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 10000;
 app.use(bodyParser.json());
 
 // Google Apps Script Web API URL
-const GAS_URL = "https://script.google.com/macros/s/AKfycbwTkz4z1ixG2j3nS7oDwnTY9_JRvQ5zg_PIVuBNkc-2uAyodsgxKIzHO1wq9h0w-gvwuA/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbwSqpl2qJqZn4pYZTR-0drQYZE825koqJJ3XoJHmh28FKm9ljlCl0IlRMX2fVG4rVQ86A/exec";
 
 // 讀取 LINE Bot 的 Token
 const LINE_ACCESS_TOKEN = process.env.LINE_ACCESS_TOKEN;
